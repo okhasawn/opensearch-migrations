2443b85141a4d46f61ebfe7ae9e8392875fd77cc
Omar Khasawneh <okhasawn@amazon.com>
2023-12-20 14:25:28 -0600

    attempting to add set -x to proxy setup script
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- test/awsE2ESolutionSetup.sh
+++ test/awsE2ESolutionSetup.sh
@@ -33,7 +33,8 @@ prepare_source_nodes_for_capture () {
       aws ec2 modify-instance-attribute --instance-id $id --groups $group_ids_string
     fi
     echo "Executing ./startCaptureProxy.sh --kafka-endpoints $kafka_brokers on node: $id. Attempting to append output of command: "
-    command_id=$(aws ssm send-command --instance-ids "$id" --document-name "AWS-RunShellScript" --parameters commands="cd /home/ec2-user/capture-proxy && sed -i '2iset -x' startCaptureProxy.sh && ./startCaptureProxy.sh --kafka-endpoints $kafka_brokers" --output text --query 'Command.CommandId')
+    command_id=$(aws ssm send-command --instance-ids "$id" --document-name "AWS-RunShellScript" --parameters "commands=cd /home/ec2-user/capture-proxy && sed -i '2iset -x' startCaptureProxy.sh && ./startCaptureProxy.sh --kafka-endpoints $kafka_brokers" --output text --query 'Command.CommandId')
+
     sleep 5
     echo "Standard Output:"
     aws ssm get-command-invocation --command-id "$command_id" --instance-id "$id" --output text --query 'StandardOutputContent'
